eea35e391e7d4d2fb40b880c0e65e1d9317601ab
Kevin_Gillispie <oakenquill@gmail.com>
2017-08-30 19:03:15 -0700

    init commit

---
+++ ChallengePostalCalculatorHelperMethods/PostageCalc.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostageCalc.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.PostageCalc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Postal Calculator</h1>
+    <form id="form1" runat="server">
+        <div>
+            <p>Width:</p>
+            <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="generateParcelPrice"></asp:TextBox>
+            <br />
+            <br />
+            <p>Height:</p>
+            <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="generateParcelPrice"></asp:TextBox>
+            <br />
+            <br />
+            <p>Length:</p>
+            <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="generateParcelPrice"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadio" runat="server" Text="Ground" AutoPostBack="True" GroupName="deliveryMethod" OnCheckedChanged="generateParcelPrice" />
+            <br />
+            <asp:RadioButton ID="airRadio" runat="server" Text="Air" AutoPostBack="True" GroupName="deliveryMethod" OnCheckedChanged="generateParcelPrice" />
+            <br />
+            <asp:RadioButton ID="nextDayRadio" runat="server" Text="Next Day" AutoPostBack="True" GroupName="deliveryMethod" OnCheckedChanged="generateParcelPrice" />
+            <br />
+            <br />
+            <p>Price: <asp:Label ID="resultLabel" runat="server"></asp:Label></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/PostageCalc.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class PostageCalc : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void generateParcelPrice(object sender, EventArgs e)
+        {
+            calculateVolume();
+            calculatePrice();
+        }
+
+        //====================================================================//
+
+        /*
+        double volume = 0.0;
+        double price = 0.0;
+
+        private void calculateVolume()
+        {
+            double width = Double.Parse(widthTextBox.Text.Trim());
+            double height = Double.Parse(heightTextBox.Text.Trim());
+
+            bool lengthBool = Double.TryParse(lengthTextBox.Text.Trim(), out double length);
+            
+            if (!lengthBool) volume = width * height;
+            else
+            {
+                length = Double.Parse(lengthTextBox.Text);
+                volume = width * height * length;
+            }
+
+            return;
+        }
+
+        private void calculatePrice()
+        {
+            if (groundRadio.Checked) price = volume * .15;
+            else if (airRadio.Checked) price = volume * .25;
+            else price = volume * .45;
+            resultLabel.Text = "$" + price.ToString();
+        }
+        */
+        //====================================================================//
+
+        double volume;
+
+        private void calculateVolume()
+        {
+
+            if (widthTextBox.Text.Trim().Length == 0 
+                || heightTextBox.Text.Trim().Length == 0
+                || !Double.TryParse(widthTextBox.Text, out double width)
+                || !Double.TryParse(heightTextBox.Text, out double height))
+            {
+                resultLabel.Text = "Please enter both width and height.";
+                return;
+            }
+            else if (lengthTextBox.Text.Trim().Length == 0
+                || !Double.TryParse(lengthTextBox.Text, out double length))
+            {
+                volume = width * height;
+                return;
+            }
+            else volume = width * height * length;
+            return;
+        }
+
+        private void calculatePrice()
+        {
+            double price;
+
+            if (groundRadio.Checked) price = volume * .15;
+            else if (airRadio.Checked) price = volume * .25;
+            else if (nextDayRadio.Checked) price = volume * .45;
+            else return;
+
+            resultLabel.Text = "$" + price.ToString();
+        }
+
+    }
+}
